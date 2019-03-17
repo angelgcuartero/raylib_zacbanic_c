3c22f0812ce749d1c73ab41e74a2f0b4625ee4d1
Angel G. Cuartero <anhelido@gmail.com>
2019-03-17 20:14:19 +0100

    Textures OK. Ship, asteroids and aliens move.

--- zacbanic.c
+++ zacbanic.c
@@ -1,45 +1,141 @@
 /*******************************************************************************************
-*
-*   raylib - Simple Game template
-*
-*   <Game title>
-*   <Game description>
-*
-*   This game has been created using raylib (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2014-2018 Ramon Santamaria (@raysan5)
-*
+*   Zacbanic
+*   Sord M5 computer came with a game in tape named Zacbanic. This a Zacbanic clone.
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+#define FACTOR 4
+#define ASTER_NUM 8
+#define ALIEN_NUM 4
+
+// User-defined types.
+typedef enum GameScreen { TITLE=0, GAMEPLAY, ENDING } GameScreen;
+typedef enum Direction { UP = 0, DOWN } Direction;
+
+// Global variables.
+Rectangle ship;
+Rectangle asteroids[ASTER_NUM];
+Rectangle aliens[ALIEN_NUM];
+
+Texture2D sprites;
+
+// Locate individual textures.
+Rectangle shipText = {0, 0, 14*FACTOR, 16*FACTOR};
+Rectangle alienText = {14*FACTOR, 0, 12*FACTOR, 16*FACTOR};
+Rectangle asteroidText = {(14+12)*FACTOR, 0, 8*FACTOR, 14*FACTOR};
+Rectangle livesText = {(14+12+8)*FACTOR, 0, 8*FACTOR, 8*FACTOR};
+Rectangle ashotText = {(14+12+8+8)*FACTOR, 0, 3*FACTOR, 3*FACTOR};
+Rectangle sshotText = {(14+12+8+8)*FACTOR, 3*FACTOR, 3*FACTOR, 10*FACTOR};
+
+GameScreen currentScreen = TITLE;
+int screenWidth = 256*FACTOR;
+int screenHeight = 192*FACTOR;
+
+
+// Prototypes
+// ----------
+void InitializeElements(void);
+void LoadGameTextures(void);
+void InitShip(void);
+void InitAsteroids(void);
+void InitAliens(void);
+void DrawShip(void);
+void DrawAsteroids(void);
+void DrawAliens(void);
+void MoveAsteroids(void);
+void MoveAliens(void);
+
+
+// Initialize window and primary game elements.
+// --------------------------------------------
+void InitializeElements(void)
+{
+    InitWindow(screenWidth, screenHeight, "Zacbanic");
+    // ToggleFullscreen();
+    LoadGameTextures();
+    InitShip();
+    InitAsteroids();
+    InitAliens();
+    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
+}
+
+void LoadGameTextures(void)
+{
+    char spriteFile[128];
+    sprintf(spriteFile, "./res/SpritesZacbanicx%1d.png", FACTOR);
+    sprites = LoadTexture(spriteFile);
+}
+
+void InitShip(void)
+{
+    ship = (Rectangle) {screenWidth/2, screenHeight - shipText.height, shipText.width, shipText.height};
+}
+
+void InitAsteroids(void)
+{
+    for (int i=0; i<ASTER_NUM; ++i)
+        asteroids[i] = (Rectangle) {GetRandomValue(0, screenWidth), GetRandomValue(0, screenHeight), 16*FACTOR, 16*FACTOR};
+}
+
+void InitAliens(void)
+{
+    for (int i=0; i<ALIEN_NUM; ++i)
+        aliens[i] = (Rectangle) {GetRandomValue(0, screenWidth), GetRandomValue(0, screenHeight), 16*FACTOR, 16*FACTOR};
+}
+
+void DrawShip(void)
+{
+    DrawTextureRec(sprites, shipText, (Vector2) {ship.x, ship.y}, WHITE);
+}
+
+void DrawAsteroids(void)
+{
+    for (int i=0; i<ASTER_NUM; ++i)
+        DrawTextureRec(sprites, asteroidText, (Vector2) {asteroids[i].x, asteroids[i].y}, WHITE);
+}
+
+void DrawAliens(void)
+{
+    for (int i=0; i<ALIEN_NUM; ++i)
+        DrawTextureRec(sprites, alienText, (Vector2) {aliens[i].x, aliens[i].y}, WHITE);
+}
+
+void MoveAsteroids(void)
+{
+    for (int i=0; i<ASTER_NUM; ++i)
+    {
+        asteroids[i].y += FACTOR;
+        if (asteroids[i].y > screenHeight)
+        {
+            asteroids[i].x = GetRandomValue(0, screenWidth);
+            asteroids[i].y = 0 - asteroidText.height;
+        }
+    }
+}
+
+void MoveAliens(void)
+{
+    for (int i=0; i<ALIEN_NUM; ++i)
+    {
+        aliens[i].y += FACTOR;
+        if (aliens[i].y > screenHeight)
+        {
+            aliens[i].x = GetRandomValue(0, screenWidth);
+            aliens[i].y = 0 - alienText.height;
+        }
+    }
+}
+
 
 //----------------------------------------------------------------------------------
 // Main entry point
 //----------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization (Note windowTitle is unused on Android)
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 256*4;
-    const int screenHeight = 192*4;
-    InitWindow(screenWidth, screenHeight, "raylib template - simple game");
-    ToggleFullscreen();
-
-    GameScreen currentScreen = LOGO;
-
-    // TODO: Initialize all required variables and load all required data here!
-
-    int framesCounter = 0;          // Useful to count frames
-
-    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
-    //--------------------------------------------------------------------------------------
-
+    InitializeElements();
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -47,18 +143,6 @@ int main(void)
         //----------------------------------------------------------------------------------
         switch(currentScreen)
         {
-            case LOGO:
-            {
-                // TODO: Update LOGO screen variables here!
-
-                framesCounter++;    // Count frames
-
-                // Wait for 2 seconds (120 frames) before jumping to TITLE screen
-                if (framesCounter > 120)
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
             case TITLE:
             {
                 // TODO: Update TITLE screen variables here!
@@ -72,12 +156,14 @@ int main(void)
             case GAMEPLAY:
             {
                 // TODO: Update GAMEPLAY screen variables here!
+                MoveAsteroids();
+                MoveAliens();
 
-                // Press enter to change to ENDING screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = ENDING;
-                }
+                // Check keys.
+                if (IsKeyDown(KEY_O) || IsKeyDown(KEY_LEFT))
+                    ship.x -= FACTOR;
+                if (IsKeyDown(KEY_P) || IsKeyDown(KEY_RIGHT))
+                    ship.x += FACTOR;
             } break;
             case ENDING:
             {
@@ -97,37 +183,32 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
             switch(currentScreen)
             {
-                case LOGO:
-                {
-                    // TODO: Draw LOGO screen here!
-                    DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
-                    DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
-
-                } break;
                 case TITLE:
                 {
                     // TODO: Draw TITLE screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-                    DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                    DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+                    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), GREEN);
+                    // DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                    DrawText("ZACBANIC", 20, 20, 40*FACTOR, DARKGREEN);
+                    DrawText("PRESS ENTER to PLAY", 20, 80*FACTOR, 10*FACTOR, DARKGREEN);
+                    DrawText("PRESS ESC to QUIT", 20, 80*FACTOR + 10*FACTOR, 10*FACTOR, DARKGREEN);
 
                 } break;
                 case GAMEPLAY:
                 {
                     // TODO: Draw GAMEPLAY screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-                    DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
-                    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
-
+                    DrawShip();
+                    DrawAsteroids();
+                    DrawAliens();
                 } break;
                 case ENDING:
                 {
                     // TODO: Draw ENDING screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLUE);
+                    // DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
                     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
                     DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
 
